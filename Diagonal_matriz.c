9eccd33b6b5eb3bde268e02efa24776bf1786713
Mattedlopez <130605448+Mattedlopez@users.noreply.github.com>
2023-05-22 20:41:19 -0500

    commit 1

---
+++ Diagonal_matriz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main (){
+    int matriz [20][20];
+    int i, j;
+    int rows, columns;
+
+    printf("Ingresa la cantidad de filas que quieres: ");
+    scanf("%d", &rows);
+    printf("Ingresa la cantidad de columnas que quieres: ");
+    scanf("%d", &columns);
+
+    
+
+
+
+    return 0;
+}
